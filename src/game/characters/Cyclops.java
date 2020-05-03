21f3db889f0b278c9a3e199f43a646570bfa0376
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-03 15:12:53 -0500

    Added Cyclops character

---
+++ src/game/characters/Cyclops.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Cyclops extends Character {
+
+	public Cyclops() {
+		super(CharacterType.Creature, CharacterName.Cyclops, 9, 2, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
