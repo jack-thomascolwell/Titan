1b6df5a89e16c66de732599babedf4bb8dc82add
casey <fordcb@lafayette.edu>
2020-05-14 15:33:37 -0500

    ok NOW muster is done... thats probably it from me today

--- src/game/actions/Muster.java
+++ src/game/actions/Muster.java
@@ -2,21 +2,28 @@ package game.actions;
 
 import game.masterboard.Legion;
 import game.masterboard.lands.Land;
+import game.characters.Character;
 
 public class Muster extends BoardAction{
 
+    private Character new_troop;
     /**
      * land is the land the legion is on.
      * legion is the legion being mustered.
      * @param legion
      */
-    public Muster(Legion legion) {
+    public Muster(Legion legion, Character troop) {
         //land = legion.getLand();
         this.legion = legion;
+        new_troop = troop;
     }
 
     @Override
+    /**
+     * prompts legion to add a new troop, returns if it succeeds
+     * @return troop_added
+     */
     public boolean preformAction() {
-        return false;
+        return legion.add(new_troop);
     }
 }
