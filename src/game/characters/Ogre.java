6c290e1b5c58402d687cf05bdd16b4536f6f8c1b
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:20:32 -0500

    Added Ogre character

---
+++ src/game/characters/Ogre.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Ogre extends Character {
+
+	public Ogre() {
+		super(CharacterType.Creature, CharacterName.Ogre, 6, 2, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
