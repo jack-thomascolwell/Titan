d40f0f5405ac65cb4a19a7d4f4b31abca8a1cc62
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:18:39 -0500

    Added Centaur character

---
+++ src/game/characters/Centaur.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Centaur extends Character {
+
+	public Centaur() {
+		super(CharacterType.Creature, CharacterName.Centaur, 3, 4, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
