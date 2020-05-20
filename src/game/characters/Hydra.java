c6bab3b502e1cbfc1fd1af4a8d0a3c469d0d0c67
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:20:02 -0500

    Added Hydra character

---
+++ src/game/characters/Hydra.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Hydra extends Character {
+
+	public Hydra() {
+		super(CharacterType.Creature, CharacterName.Hydra, 10, 3, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
