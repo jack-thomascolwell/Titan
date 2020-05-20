15771c833871a18b4b58a8a2efc1d9316b808f92
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:21:38 -0500

    Added Warlock character

---
+++ src/game/characters/Warlock.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Warlock extends Character {
+
+	public Warlock() {
+		super(CharacterType.Demilord, CharacterName.Warlock, 5, 4, false, true);
+		// TODO Auto-generated constructor stub
+	}
+
+}
