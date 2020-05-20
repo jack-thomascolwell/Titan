96477da77b4398676d2b2fdcedc8aea93ab16ac9
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:20:42 -0500

    Added Ranger character

---
+++ src/game/characters/Ranger.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Ranger extends Character {
+
+	public Ranger() {
+		super(CharacterType.Creature, CharacterName.Ranger, 4, 4, true, true);
+		// TODO Auto-generated constructor stub
+	}
+
+}
