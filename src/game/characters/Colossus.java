4413427fea85e4d21cc07823e6dd9ff3a27f314e
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2020-05-20 16:19:01 -0500

    Added Colossus character

---
+++ src/game/characters/Colossus.java
@@ -0,0 +1,10 @@
+package game.characters;
+
+public class Colossus extends Character {
+
+	public Colossus() {
+		super(CharacterType.Creature, CharacterName.Colossus, 10, 4, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
